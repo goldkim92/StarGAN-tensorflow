3b8e1cbaca99daa52f759befda2c3fcedd76b93d
goldkim92 <goldkim92@gmail.com>
2017-12-11 21:39:34 +0900

    third commit

--- model.py
+++ model.py
@@ -2,13 +2,14 @@ import os
 import numpy as np
 import tensorflow as tf
 from collections import namedtuple
+from tqdm import tqdm
 
 from module import generator, discriminator, sce_loss, recon_loss
-from util import load_data_list
+from util import load_data_list, attr_extract, preprocess_attr, preprocess_image, preprocess_input
 
 class stargan(object):
     def __init__(self,sess,args):
-        __data_dir = os.path.join('.','data','celebA','train')
+        __data_dir = os.path.join('.','data','celebA')
         __log_dir = os.path.join('.','assets','log')
         __ckpt_dir = os.path.join('.','assets','checkpoint')
         __epoch = 100
@@ -54,6 +55,9 @@ class stargan(object):
         self.real_B = tf.placeholder(tf.float32, 
                                            [None, self.image_size, self.image_size, self.image_channel + self.n_labels],
                                            name = 'target_images')
+        self.fake_B_sample = tf.placeholder(tf.float32,
+                                            [None, self.image_size, self.image_size, self.image_channel],
+                                            name = 'fake_images_sample')
         
         # generate image
         self.fake_B = generator(self.real_A, self.options, False, name='gen')
@@ -61,8 +65,9 @@ class stargan(object):
         
         # discriminate image
         # src: real or fake, cls: domain classification 
-        self.src_real_B, self.cls_real_B = discriminator(self.real_B, self.options, False, name='disc')
-        self.src_fake_B, self.cls_fake_B = discriminator(self.fake_B, self.options, True, name='disc')
+        self.src_real_B, self.cls_real_B = discriminator(self.real_B[:,:,:,:self.image_channel], 
+                                                         self.options, False, name='disc')
+        self.src_fake_B, self.cls_fake_B = discriminator(self.fake_B_sample, self.options, True, name='disc')
         
         # loss
         ## discriminator loss ##
@@ -98,37 +103,68 @@ class stargan(object):
     def train(self):
         # summary setting
         self.summary()
-        # load train data
-        dataA_list = load_data_list(self.data_dir)
-        dataB_list = dataA_list
+        
+        # load train data list & load attribute data
+        dataA_files = load_data_list(self.data_dir)
+        dataB_files = np.copy(dataA_files)
+        attr_names, attr_list = attr_extract(self.data_dir)
+        
         # variable initialize
         self.sess.run(tf.global_variables_initializer())
+        
         # load or not checkpoint
         if self.continue_train and self.checkpoint_load():
             print(" [*] before training, Load SUCCESS ")
         else:
             print(" [!] before training, no need to Load ")
         
-        batch_idxs = len(dataA_list) #182599
+        batch_idxs = len(dataA_files) // self.batch_size # 182599
         #train
         for epoch in range(self.epoch):
-            np.random.shuffle(dataA_list)
-            np.random.shuffle(dataB_list)
+            np.random.shuffle(dataA_files)
+            np.random.shuffle(dataB_files)
             
             for idx in tqdm(range(batch_idxs)):
+                # 
+                dataA_list = dataA_files[idx * self.batch_size : (idx+1) * self.batch_size]
+                dataB_list = dataB_files[idx * self.batch_size : (idx+1) * self.batch_size]
+                attrA_list = [attr_list[os.path.basename(val)] for val in dataA_list]
+                attrB_list = [attr_list[os.path.basename(val)] for val in dataB_list]
+                # get batch images and labels
+                attrA, attrB = preprocess_attr(attr_names, attrA_list, attrB_list)
+                imgA, imgB = preprocess_image(dataA_list, dataB_list, self.image_size)
+                dataA, dataB = preprocess_input(imgA, imgB, attrA, attrB, self.image_size)
+                
+                # updatae G network
+                feed = { self.real_A: dataA, self.real_B: dataB }
+                fake_B, _, summary = self.sess.run([self.fake_B, self.g_optim, self.g_sum], 
+                                           feed_dict = feed)
+                
+                #update D network
+                feed = { self.real_B: dataB, self.fake_B_sample: fake_B }
+                _, summary = self.sess.run([self.d_optim, self.d_sum])
+                
+                # display(summary) and save
                 
             
-        
-    
-    
+   
     def summary(self):
         # summary writer
         self.writer = tf.summary.FileWriter(self.log_dir, self.sess.graph)
         
-        # session
-        tf.summary.scalar('d_loss', self.d_loss)
-        tf.summary.scalar('g_loss', self.g_loss)
-        self.summary = tf.summary.merge_all()
+        # session : discriminator
+        sum_d_1 = tf.summary.scalar('d_real_adv_loss', self.d_real_adv_loss)
+        sum_d_2 = tf.summary.scalar('d_fake_adv_loss', self.d_fake_adv_loss)
+        sum_d_3 = tf.summary.scalar('d_real_cls_loss', self.d_real_cls_loss)
+        sum_d_4 = tf.summary.scalar('d_loss', self.d_loss)
+        self.d_sum = tf.summary.scalar([sum_d_1, sum_d_2, sum_d_3, sum_d_4])
+        
+        # session : generator
+        sum_g_1 = tf.summary.scalar('g_fake_adv_loss', self.g_fake_adv_loss)
+        sum_g_2 = tf.summary.scalar('g_fake_cls_loss', self.g_fake_cls_loss)
+        sum_g_3 = tf.summary.scalar('g_recon_loss', self.g_recon_loss)
+        sum_g_4 = tf.summary.scalar('g_loss', self.g_loss)
+        self.g_sum = tf.summary.scalar([sum_g_1, sum_g_2, sum_g_3, sum_g_4])
        
     
     def checkpoint_load(self):
--- util.py
+++ util.py
@@ -1,47 +1,88 @@
-#%% import
 import os
 from glob import glob
 from collections import namedtuple
+import numpy as np
+import scipy.misc as scm
+import matplotlib.pyplot as plt
 
 
-#%%
 def load_data_list(data_dir):
-    path = os.path.join(data_dir,'*')
+    path = os.path.join(data_dir, 'train', '*')
     file_list = glob(path)
     return file_list
 
-#%% file and directory (csv txt file)
-def attr_extract():
-    attr = {}
+
+def attr_extract(data_dir):
+    attr_list = {}
     
-    path = os.path.join('.','data', 'list_attr_celeba.txt')
+    path = os.path.join(data_dir, 'list_attr_celeba.txt')
     file = open(path,'r')
     
     n = file.readline()
     n = int(n.split('\n')[0]) #  # of celebA img: 202599
     
     attr_line = file.readline()
-    attrs = attr_line.split('\n')[0].split() # attribute name
-    attrs[0] = 'Clock_Shadow' # originally it was 5_o_ Clock_Shadow, but in order to put it in namedtuple, we must delete the number
-    
-    ATTRS = namedtuple('ATTRS', [attr for attr in attrs])
+    attr_names = attr_line.split('\n')[0].split() # attribute name
     
     for line in file:
         row = line.split('\n')[0].split()
         img_name = row.pop(0)
-        row = ATTRS(*row)
-        attr[img_name] = row   
+        row = [int(val) for val in row]
+#    img = img[..., ::-1] # bgr to rgb
+        attr_list[img_name] = row   
     
     file.close()
-    return attr
+    return attr_names, attr_list
 
 
-Black Hair
-Blond Hair
-Brown Hair
-Gray Hair
-Male
-Young
-Mustache
-No_Beard
-Eyeglasses
+def preprocess_attr(attr_names, attrA_list, attrB_list):
+    attr_keys = ['Black_Hair','Blond_Hair','Brown_Hair','Gray_Hair','Male',
+                 'Young','Mustache','No_Beard','Eyeglasses','Wearing_Hat']
+    attrA = []
+    attrB = []    
+
+    for i in range(len(attrA_list)):
+        tmpA = [attrA_list[i][attr_names.index(val)] for val in attr_keys]
+        tmpA = [1. if val == 1 else 0. for val in tmpA]
+        attrA.append(tmpA)
+        tmpB = [attrB_list[i][attr_names.index(val)] for val in attr_keys]
+        tmpB = [1. if val == 1 else 0. for val in tmpB]
+        attrB.append(tmpB)
+
+    return attrA, attrB
+
+
+def preprocess_image(dataA_list, dataB_list, image_size):
+    imgA = [get_image(img_path, image_size) for img_path in dataA_list]
+    imgA = np.array(imgA)
+    
+    imgB = [get_image(img_path, image_size) for img_path in dataB_list]
+    imgB = np.array(imgB)
+    return imgA, imgB
+        
+    
+def preprocess_input(imgA, imgB, attrA, attrB, image_size, n_label):
+    # dataA = imgA + attrB , dataB = imgB + attrA
+    attrA = np.tile(np.reshape(attrA, [-1,1,1,n_label]),[1,image_size,image_size,1])
+    attrB = np.tile(np.reshape(attrB, [-1,1,1,n_label]),[1,image_size,image_size,1])
+    dataA = np.concatenate((imgA, attrB), axis=3)
+    dataB = np.concatenate((imgB, attrA), axis=3)
+    return dataA, dataB
+
+
+def get_image(img_path, data_size):
+    img = scm.imread(img_path)
+    img_crop = img[15:203,9:169,:]
+    img_resize = scm.imresize(img_crop,[data_size,data_size,3])
+    img_resize = img_resize/127.5 - 1.
+    
+    return img_resize
+
+
+def inverse_image(img):
+    img = (img + 1.) * 127.5
+    img[img > 255] = 255.
+    img[img < 0] = 0.
+    return img.astype(np.uint8)
+
+
