796ddf1ac537a80a49dbe97f29f698e33eee4906
goldkim92 <goldkim92@gmail.com>
2017-12-13 20:25:41 +0900

    tf.square -> tf.sqrt

--- module.py
+++ module.py
@@ -56,8 +56,8 @@ def discriminator(images, options, reuse=False, name='disc'):
 def wgan_gp_loss(real_img, fake_img, options, epsilon):
     hat_img = epsilon * real_img + (1-epsilon) * fake_img
     gradients = tf.gradients(discriminator(hat_img, options, reuse=True, name='disc')[0], xs=[hat_img])[0]
-    slopes = tf.square(tf.reduce_sum(tf.square(gradients), axis=[1,2,3]))
-    gradient_penalty = tf.reduce_mean((slopes - 1.)**2)
+    slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1,2,3]))
+    gradient_penalty = tf.reduce_mean(tf.square(slopes - 1.))
     
     return options.lambda_gp * gradient_penalty
         
